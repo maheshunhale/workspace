af85995b4b3c81e256164565157857e8e1b1300c
Mahesh Unhale <maheshunhale1@gmail.com>
2022-01-06 20:33:24 +0530

    Initial Commit:Basic structure of application

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -12,7 +12,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name="author_book", joinColumns = @JoinColumn(name="book_id"), inverseJoinColumns = @JoinColumn(name="author_id"))
@@ -21,11 +23,17 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
+    }
+
+    public Book(String title, String isbn, Publisher publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
     }
 
     public Long getId() {
@@ -52,11 +60,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
